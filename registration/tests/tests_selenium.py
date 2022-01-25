e765b67c8b95a34da9b03a43730e6a3581a62d95
JohLpe <johanna.laporte@ymail.com>
2022-01-25 08:33:38 +0100

    test selenium update

--- registration/tests/tests_selenium.py
+++ registration/tests/tests_selenium.py
@@ -1,5 +1,9 @@
 import unittest
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as cond
 from selenium.webdriver.common.by import By
 
 
@@ -7,44 +11,40 @@ class RegistrationPage(unittest.TestCase):
 
     def setUp(self):
 
-        self.driver = webdriver.Chrome(executable_path='chromedriver')
+        self.service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service)
         self.driver.get("http://127.0.0.1:8000/registration/")
-        self.wait = self.driver.implicitly_wait(10)
 
     def test_registration_page(self):
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_username"]'
                                  ).send_keys("TestUsername")
-        self.wait
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_first_name"]'
                                  ).send_keys("TestFirstname")
-        self.wait
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_last_name"]'
                                  ).send_keys("TestLastname")
-        self.wait
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_email"]'
                                  ).send_keys("TestUser@mail.com")
-        self.wait
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_password1"]'
-                                 ).send_keys("TestPassword")
-        self.wait
+                                 ).send_keys("deigibefopn465")
 
         self.driver.find_element(By.XPATH,
                                  '//*[@id="id_password2"]'
-                                 ).send_keys("TestPassword")
-        self.wait
+                                 ).send_keys("deigibefopn465")
 
-        self.driver.find_element(By.NAME, "Create user").click()
-        self.wait
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="registration-form"]/input[2]'
+                                 ).click()
+        WebDriverWait(self.driver, 5).until(cond.url_changes("http://127.0.0.1:8000/registration/login/"))
 
     def tearDown(self):
         self.driver.close()
